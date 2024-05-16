2c29055e510717ae3e433d5c57fec478edceeb87
ASwenia <148665967+ASwenia@users.noreply.github.com>
2024-05-16 20:40:05 +0000

    comit2

---
+++ server.js
@@ -0,0 +1,53 @@
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;  
+//3306
+// SSL options
+const options = {
+    key: fs.readFileSync(path.join(__dirname, 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert.pem'))
+};
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '12345678',
+    database: 'sqlscript',
+    port: '3306'
+});
+
+
+// Serve static files from the "html" directory
+app.use(express.static('html'));
+
+// Serve index.html at the root
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'html', 'index.html'));
+});
+
+// Serve index.html at the root
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'html', 'index.html'));
+});
+
+// Serve index.html at the root
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'html', 'index.html'));
+});
+
+
+// Serve index.html at the root
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'html', 'index.html'));
+});
+
+// Start the HTTPS server
+https.createServer(options, app).listen(PORT, '0.0.0.0', () => {
+    console.log(`HTTPS server running on https://localhost:${PORT}`);
+});
